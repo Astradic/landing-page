474f174c9fd54fed38bd582f8a52c7c80579d212
David Cowles <davidcowles07@gmail.com>
2025-12-08 09:18:32 -0600

    change dist to docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   srcDir: "src",
-  outDir: "dist",
+  outDir: "docs",
   base: "./",
   build: {
     assetsPrefix: "./",
